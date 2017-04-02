493070f1b12f528b1b193220c1ab357f44d9f0d6
Roman Ilyushin <irsick@gmail.com>
2017-04-02 14:15:23 -0400

    kg: data normalization logic

--- src/app/post/tracker.service.ts
+++ src/app/post/tracker.service.ts
@@ -19,12 +19,67 @@ export class TrackerService {
 
         return this.http.get(this.trackersUrl, { search: params })
             .map(this.extractData)
+            .map(trackersList => this.normalizeData(trackersList, from, to))
             .catch(this.handleError);
     }
 
     private extractData(res: Response) {
         const body = res.json();
         return body.data || { };
+    }
+
+    // NOTE: API data doesn't consider timezones
+    // NOTE: API data doesn't consider daytime shifts
+    private normalizeData(trackersList: Tracker[], from: string, to: string) {
+        const result: Tracker[] = [];
+        const end: Date = new Date(to);
+        let item: Tracker;
+        let current: Date = new Date(from);
+        console.log(from, current);
+        let dateString: string;
+        let id: number = 1;
+
+        while (current < end) {
+            dateString = this.getIsoDataString(current);
+            item = this.getItem(dateString, trackersList);
+
+            if (item !== null) {
+                result.push(item);
+            } else {
+                result.push({
+                    id: id,
+                    hits: 0,
+                    date: dateString
+                });
+            }
+
+            const newDate = current.setDate(current.getDate() + 1);
+            current = new Date(newDate);
+            id++;
+        }
+
+        return result;
+    }
+
+    // helper function to build ISO-8601 string
+    private getIsoDataString(date: Date): string {
+        const day: string = ('0' + date.getDate()).slice(-2);
+        const month: string = ('0' + (date.getMonth() + 1)).slice(-2);
+        const year: number = date.getFullYear();
+
+        return `${year}-${month}-${day}`;
+    }
+
+    // helper function to find tracker obj by date if any
+    private getItem(itemDate: string, trackersList: Tracker[]): Tracker {
+        let item: Tracker = null;
+        for (const tracker of trackersList) {
+            if (tracker['date'] === itemDate) {
+                item = tracker;
+                break;
+            }
+        }
+        return item;
     }
 
     private handleError (error: Response | any) {
